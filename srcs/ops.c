e18d2b09e7de989c9d8392b26b0bc8b60f51eb4b
Roman Politov <tinkotsu@MacBook-Pro-Roman.local>
2019-07-24 14:16:54 +0300

    12

--- srcs/ops.c
+++ srcs/ops.c
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_add.c                                           :+:      :+:    :+:   */
+/*   ft_mul.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ifran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/16 02:26:42 by ifran             #+#    #+#             */
-/*   Updated: 2019/07/20 23:19:03 by ifran            ###   ########.fr       */
+/*   Created: 2019/07/16 02:29:51 by ifran             #+#    #+#             */
+/*   Updated: 2019/07/20 23:19:14 by ifran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+int		ft_mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+int		ft_sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
 int		ft_add(int num1, int num2)
 {
 	return (num1 + num2);
+}
+
+int		ft_div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+int		ft_mod(int num1, int num2)
+{
+	return (num1 % num2);
 }
